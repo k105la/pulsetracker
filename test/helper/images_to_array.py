ea1886fa483fdaaf5dcdfc11dadfd6e88ca0dca1
akilhylton <hyltonakil@gmail.com>
2020-05-02 10:03:29 -0400

    Finished signal differentiation

--- test/helper/images_to_array.py
+++ test/helper/images_to_array.py
@@ -3,15 +3,18 @@ import cv2 as cv
 import os
 from tqdm import tqdm
 
+new_array = []
+
 def feature_images_to_array(folder):
     images = [os.path.join(root, filename)
              for root, dirs, files in os.walk(folder)
              for filename in files
              if [filename]]
-    ic = []
+    
     for img in tqdm(images):
         img_array = cv.imread(img)
         size_array = cv.resize(img_array, (320, 240), interpolation = cv.INTER_NEAREST)
-        ic.append(size_array)
-    X = np.asarray(ic)
+        size_array = cv.cvtColor(size_array, cv.COLOR_BGR2RGB)
+        new_array.append(size_array)
+    X = np.asarray(new_array)
     return X
--- test/hr_test.py
+++ test/hr_test.py
@@ -1,18 +1,28 @@
+from tqdm import tqdm
 import numpy as np
+import glob
+import cv2 as cv
 import matplotlib.pyplot as plt
-from helper.images_to_array import feature_images_to_array
 
-test_images = "./images_rgb/"
+# Signal Differentiation 
+path = glob.glob("./heartrate/test/images_rgb/*.jpg")
+cv_img = []
+for img in tqdm(path):
+    n = cv.imread(img)
+    img_array = cv.resize(n, (320, 240))
+    img_array = cv.cvtColor(img_array, cv.COLOR_RGB2GRAY)
+    cv_img.append(img_array)
 
-x = feature_images_to_array(test_images)
+c = np.asarray(cv_img)
 
-X = x.copy()
-X[:, :, 1] = 0
-X[:, :, 2] = 0
+print(c.shape)
 
-print(X.shape)
-print(X[0] - X[1])
-print(int(abs(np.mean(X[0] - X[1]))))
+avg_red = []
 
-#plt.imshow(X[0], interpolation='nearest')
-#plt.show()
+plt.figure(figsize=(20,10))
+
+for i in range(614):
+    avg_red.append(int(abs(np.mean(c[i] - c[i + 1]))))
+    signal = np.asarray(avg_red)
+
+print(signal[0])
