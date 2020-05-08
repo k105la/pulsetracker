1127e8b167a7084e49263fdff52e46607b758897
akilhylton <hyltonakil@gmail.com>
2020-05-08 15:39:38 -0400

    Added video processing speed test

--- test/test_time.py
+++ test/test_time.py
@@ -3,10 +3,22 @@ import time
 import unittest
 
 class TimeTest(unittest.TestCase):
+    def setUp(self):
+        self.startTime = time.time()
+
+    def tearDown(self):
+        t = time.time() - self.startTime
+        print(f'{self.id()}: {t}')
+
     # Test the speed of the heartrate algorithm 
-    def testOne(self):
+    def testBPM(self):
         heart = h.HeartRate()
-        heart.bpm()
+        heart.bpm() 
+
+    # Test the time it takes to get frames from an input video
+    def testVideoProcessing(self):
+        heart = h.HeartRate()
+        heart.video_to_frames()
 
 if __name__ == '__main__':
     time_test = unittest.TestLoader().loadTestsFromTestCase(TimeTest)
