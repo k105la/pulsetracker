d1bea2f3638aca8e17aff7c12349e36ab87a0890
Akil M Hylton <hyltonakil@gmail.com>
2020-06-03 23:54:51 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ author = 'Akil M Hylton'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+master_doc = 'index'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon' ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -36,7 +37,6 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
 
 # -- Options for HTML output -------------------------------------------------
 
