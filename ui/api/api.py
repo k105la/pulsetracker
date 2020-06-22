7df11ffdb1d6a824705e2a7d3b49820e9d9acfce
akilhylton <hyltonakil@gmail.com>
2020-06-21 20:02:15 -0400

    Remove images folder after processing heartrate

--- ui/api/api.py
+++ ui/api/api.py
@@ -1,3 +1,4 @@
+import os
 import time 
 from flask import Flask
 from flask_cors import CORS
@@ -11,6 +12,7 @@ def view_heartrate(uid):
     pulse = p.Pulse()
     pulse.pulsebox_to_frames(uid)
     hr = pulse.bpm()
+    os.system("rm -rf images")
     return f'{hr}'
 
 
