387f609a27f4b8307e656cb870e8aa56bae6059b
akilhylton <hyltonakil@gmail.com>
2020-06-24 12:12:09 -0400

    Finished porting PulseBox to React

--- ui/src/components/Home/Home.js
+++ ui/src/components/Home/Home.js
@@ -7,10 +7,21 @@ import logo from './images/LOGO.png'
 import IconButton from '@material-ui/core/IconButton';
 import PhotoCamera from '@material-ui/icons/PhotoCamera';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-
+import CircularProgress from '@material-ui/core/CircularProgress';
 import "./Home.css"
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      hr: 0,
+      prog: 0
+      }
+      this.retrievePulse = this.retrievePulse.bind(this);
+      this.uploadVideoToFirebase = this.uploadVideoToFirebase.bind(this);
+    }
+  
   componentDidMount() { 
     firebase.auth().onAuthStateChanged((user) => {
       if (user) {
@@ -21,8 +32,49 @@ class Home extends Component {
     })
  }
 
- uploadToFirebase() {
-   
+ uploadVideoToFirebase(event) {
+  const storage = firebase.storage().ref();
+  let file = event.target.files[0];
+  let blob = file.slice(0, file.size, "video/quicktime");
+  let newFile = new File([blob], "hr_test.MOV", { type: "video/quicktime" });
+
+  console.log(newFile);
+  firebase.auth().onAuthStateChanged((user) => {
+    if(user) {
+      let pulseBoxStorage = storage.child("data/" + user.uid);
+      pulseBoxStorage.listAll().then((res) => {
+          res.prefixes.forEach((folderRef) => {
+            // console.log(folderRef);
+          }) 
+        
+          res.items.forEach((itemRef) => {
+            let pulseBoxCurrentStorage = storage.child(itemRef.location.path_);
+            pulseBoxCurrentStorage.delete().then(() => {
+              console.log("Cleaning " + user.uid + " storage box.")
+            });
+          });
+
+      });
+
+      let pulseBoxData = storage.child("data/" + user.uid + "/" + newFile.name);
+      pulseBoxData.put(newFile).on("state_changed", (snapshot) => {
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        this.setState({prog: progress})
+        if (progress === 100) {
+          this.setState({prog: 0})
+        }
+        console.log(this.state.prog)
+      })
+
+
+
+    }
+
+
+  })
+ }
+
+ retrievePulse() {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       axios.get(`http://127.0.0.1:5000/${user.uid}`)
@@ -36,26 +88,24 @@ class Home extends Component {
      }
   })
  }
-  constructor(props) {
-  super(props);
-  this.state = {
-    user: {},
-    hr: 0
-    }
-    this.uploadToFirebase = this.uploadToFirebase.bind(this);
-  }
+
   render() {
+    const progessValue = this.state.prog;
   return (
 
 
   <div className="container">
-  <img
+      <img
         src={logo}
         width="100%"
         height="50%"
         className="pulse d-inline-block align-top"
-        alt="React Bootstrap logo"
+        alt="PulseTracker logo"
       />
+    { progessValue 
+    ? <div className="progress-circle"> <CircularProgress variant="static" value={this.state.prog} /> </div>
+    : <div>
+
        <h2> Welcome, {this.state.user.displayName} </h2>
 
       <p className="top-text">Let's check your pulse! </p>
@@ -63,7 +113,7 @@ class Home extends Component {
 
   <h1 className="heartrateData"> {this.state.hr}</h1>
   <p> <b> Heart Beats per Minute </b><br/> were logged by the algorithm</p>
-  <input accept="image/*" className="upload-input" id="icon-button-file" type="file" />
+  <input accept="video/*" onChange={this.uploadVideoToFirebase} className="upload-input" id="icon-button-file" type="file" ref={(ref) => this.fileUpload = ref}/>
       <label htmlFor="icon-button-file" className="camara-button">
         <IconButton color="default" aria-label="upload picture" component="span">
           <PhotoCamera />
@@ -74,18 +124,18 @@ class Home extends Component {
         className="upload-button"
         variant="contained"
         color="default"
-        onClick={this.uploadToFirebase}
+        onClick={this.retrievePulse}
         startIcon={<CloudUploadIcon />}
       >
         Upload
       </Button>
       
       <Button id="sign-out-button" onClick={() => app.auth().signOut()}>Sign out</Button>
-
-
-    </div>
-  );
+  </div>
   }
+    </div>
+    );
+  }  
 };
 
 export default Home;
