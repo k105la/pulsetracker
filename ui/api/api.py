c088c991f80574722237461e5452339953ec6c76
akilhylton <hyltonakil@gmail.com>
2020-06-30 12:59:20 -0400

    Dockerized pulsetracker api

--- ui/api/api.py
+++ ui/api/api.py
@@ -1,10 +1,14 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import pulse as p
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def welcome_screen():
+    return render_template('index.html')
 
 @app.route('/<string:uid>', methods=['GET'])
 def view_heartrate(uid):
