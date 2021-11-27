29ae9af44659e822bf1b8e9c0e4994b8e5a90f1d
akilhylton <hyltonakil@gmail.com>
2021-11-27 09:06:05 -0500

    Changed docker image version to latest

--- run_docker_test.sh
+++ run_docker_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -t pulsetracker:0.1 .
-docker run --rm -i pulsetracker:0.1 sh -c "make test"
+docker build -t pulsetracker:latest .
+docker run --rm -i pulsetracker:latest sh -c "make test"
